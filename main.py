9ac70404fb599a05ca75f9b1fc32069f35f17c21
strajk- <stevenbibi@hotmail.com>
2017-07-20 15:04:15 +0200

    Repeat Action if not Deleted
    Repeats deletion of the same Message ID if the Request Code wasn't 204 (Success)
    - Delay between deletion increased to 2 seconds (Discord kept not allowing me to delete entries)
    - IF deletion failed, wait for 5 seconds (Discord's Rate Limit Timeout is pretty high)

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ print """
 print "--- 11's discord deleter thing ---"
 print "clears all of your messages in a channel"
 print "in order for this script to work properly the channel id, auth token, and username is required"
-username = raw_input("username:")
-auth_token = raw_input("auth token:")
-channel_id = raw_input("channel id:")
+username = raw_input("username: ")
+auth_token = raw_input("auth token: ")
+channel_id = raw_input("channel id: ")
 delete_from_all_users = True if raw_input("delete messages from other users (y/n): ") == "y" else False
 
 def get_all_messages(auth, id, last="", prev=[]): # recursively find all messages in a channel, 100 at a time
@@ -35,15 +35,30 @@ def get_all_messages(auth, id, last="", prev=[]): # recursively find all message
 def delete_all(auth, id, user, messages):
     print "deleting all messages in " + id + " from username " + user
     for message in messages:
-        time.sleep(0.3) 
+        time.sleep(2.0) 
         if delete_from_all_users:
-            print "message "+ message["id"] + " deleting..."
-            print requests.delete("http://canary.discordapp.com/api/v6/channels/" + id + "/messages/" + message["id"], headers={"authorization": auth})
+            isDeleted = False
+            
+            while isDeleted == False:
+                print "message "+ message["id"] + " deleting..."
+                n = requests.delete("http://canary.discordapp.com/api/v6/channels/" + id + "/messages/" + message["id"], headers={"authorization": auth})
+                print n
+                if n.status_code == 204:
+                    isDeleted = True
+                else:
+                    time.sleep(5.0) 
         else:
             print "message "+ message["id"] + " deleting..."
             if (message["author"]["username"] == user):
-                requests.delete("http://canary.discordapp.com/api/v6/channels/" + id + "/messages/" + message["id"],
-                                headers={"authorization": auth})
+                isDeleted = False
+            
+                while isDeleted == False:
+                    n = requests.delete("http://canary.discordapp.com/api/v6/channels/" + id + "/messages/" + message["id"], headers={"authorization": auth})
+                    print n
+                    if n.status_code == 204:
+                        isDeleted = True
+                    else:
+                        time.sleep(5.0) 
     print "all messages deleted"
 
 delete_all(auth_token, channel_id, username, get_all_messages(auth_token, channel_id))
